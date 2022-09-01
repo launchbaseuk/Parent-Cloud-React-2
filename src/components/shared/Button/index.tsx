c1e035f5b84f9d33bf077e3eb6657d8b851463b4
dusanborne <dusan@borneagency.com>
2022-09-01 13:58:40 +0200

    added typescript

---
+++ src/components/shared/Button/index.tsx
@@ -0,0 +1,15 @@
+import {Button as UIButton} from '@rneui/themed';
+
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Button = () => {
+  return (
+    <View>
+      {/* <UIButton /> */}
+      <Text>Test Component</Text>
+    </View>
+  );
+};
+
+export default Button;
---
+++ src/screens/DusanScreen/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View} from 'react-native';
+
+import Button from '../../components/shared/Button';
+
+const DusanScreen = () => {
+  return (
+    <View>
+      <Button />
+    </View>
+  );
+};
+
+export default DusanScreen;
