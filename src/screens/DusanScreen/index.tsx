0c4d02e3284d1a28fab63b537444e89b221e3bb1
dusanborne <dusan@borneagency.com>
2022-09-27 12:58:14 +0200

    added sound library and some of ui

--- src/screens/DusanScreen/index.tsx
+++ src/screens/DusanScreen/index.tsx
@@ -1,23 +1,96 @@
-import React from 'react';
-import {View} from 'react-native';
-import EmailSent from '../../components/auth/EmailSent';
-import ForgotPassword from '../../components/auth/ForgotPassword';
-import Login from '../../components/auth/Login';
-import Signup from '../../components/auth/Signup';
-import Calendar from '../../components/Calendar';
-import Search from '../../screens/Search';
-import LiveTalks from '../LiveTalks';
-import OnetooneSessions from '../OnetooneSessions';
-import Payment from '../Payment';
-import RedeemCode from '../RedeemCode';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {Slider, Text as TextStyled, Icon} from '@rneui/themed';
+
+import {Switch} from '@rneui/themed';
 
 const DusanScreen = () => {
+  const [value, setValue] = useState(0);
+  const [vertValue, setVertValue] = useState(0);
+
+  const interpolate = (start: number, end: number) => {
+    let k = (value - 0) / 10; // 0 =>min  && 10 => MAX
+    return Math.ceil((1 - k) * start + k * end) % 256;
+  };
+
+  const color = () => {
+    let r = interpolate(255, 0);
+    let g = interpolate(0, 255);
+    let b = interpolate(0, 0);
+    return `rgb(${r},${g},${b})`;
+  };
+
   return (
-    <View style={{flex: 1}}>
-      <Search />
+    <SafeAreaView style={styles.wrapper}>
+      {/* <Search /> */}
       {/* <LiveTalks /> */}
-    </View>
+      {/* <Player /> */}
+      <Text style={styles.title}>Meditation</Text>
+      <Text style={styles.desc}>Lorem ipsum...</Text>
+
+      <Switch />
+      <View style={[styles.contentView]}>
+        <Slider
+          value={value}
+          onValueChange={setValue}
+          maximumValue={10}
+          minimumValue={0}
+          step={1}
+          allowTouchTrack
+          trackStyle={{height: 5, backgroundColor: 'transparent'}}
+          thumbStyle={{height: 20, width: 20, backgroundColor: 'transparent'}}
+          thumbProps={{
+            children: (
+              <Icon
+                name="heartbeat"
+                type="font-awesome"
+                size={20}
+                reverse
+                containerStyle={{bottom: 20, right: 20}}
+                color={color()}
+              />
+            ),
+          }}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 25,
+  },
+  desc: {
+    fontSize: 16,
+  },
+
+  contentView: {
+    padding: 20,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'stretch',
+  },
+  verticalContent: {
+    padding: 20,
+    flex: 1,
+    flexDirection: 'row',
+    height: 500,
+    justifyContent: 'center',
+    alignItems: 'stretch',
+  },
+  subHeader: {
+    backgroundColor: '#2089dc',
+    color: 'white',
+    textAlign: 'center',
+    paddingVertical: 5,
+    marginBottom: 10,
+  },
+});
 
 export default DusanScreen;
