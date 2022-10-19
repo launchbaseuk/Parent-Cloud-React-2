384617ffe896ba43a5ae4257f4789e6b6c5952ef
dusanborne <dusan@borneagency.com>
2022-10-19 10:48:59 +0200

    app updated

--- App.tsx
+++ App.tsx
@@ -110,12 +110,10 @@ const App = () => {
       {!loggedIn ? (
         <Stack.Navigator
           screenOptions={{headerShown: false}}
-          initialRouteName="ReviewSection">
+          initialRouteName="Onboarding">
           <Stack.Screen name="Login" component={Login} />
           <Stack.Screen name="Signup" component={Signup} />
           <Stack.Screen name="Onboarding" component={Onboarding} />
-          <Stack.Screen name="ReviewDetails" component={ReviewDetails} />
-          <Stack.Screen name="ReviewSection" component={ReviewSection} />
           <Stack.Screen name="Success" component={Success} />
           <Stack.Screen name="EmailSent" component={EmailSent} />
           <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
@@ -150,6 +148,8 @@ const App = () => {
           <Stack.Screen name="PocketCBTIntro" component={PocketCBTIntro} />
           <Stack.Screen name="FeelingsCBT" component={FeelingsCBT} />
           <Stack.Screen name="Feelings" component={Feelings} />
+          <Stack.Screen name="ReviewDetails" component={ReviewDetails} />
+          <Stack.Screen name="ReviewSection" component={ReviewSection} />
           <Stack.Screen name="Activities" component={Activities} />
           <Stack.Screen name="Thoughts" component={Thoughts} />
           <Stack.Screen name="Learn" component={Learn} />
