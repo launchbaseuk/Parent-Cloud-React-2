639031ef5e60d65ecf53aecb278846b1df31f1c3
Marios Kyriacou <60070568+imdarkk@users.noreply.github.com>
2022-10-31 14:39:03 +0200

    changes

--- src/components/shared/VideoPlayer/index.tsx
+++ src/components/shared/VideoPlayer/index.tsx
@@ -5,33 +5,36 @@ import VideoPlayer from 'react-native-video-controls';
 import {useNavigation} from '@react-navigation/native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
-const VideoPlayerShared = () => {
+const VideoPlayerShared = ({ route }: any) => {
   const [video, setVideo] = useState({
     thumbnailUrl: '',
     videoUrl: '',
     video: '',
   });
   const navigation = useNavigation();
+  console.log(route.params.filepath.videoId)
 
   useEffect(() => {
-    const VIMEO_ID = '399030109';
-    fetch(`https://player.vimeo.com/video/${VIMEO_ID}/config`)
-      .then(res => res.json())
-      .then(res =>
-        setVideo({
-          thumbnailUrl: res.video.thumbs['640'],
-          videoUrl:
-            res.request.files.hls.cdns[res.request.files.hls.default_cdn].url,
-          video: res.video,
-        }),
-      );
+    (async() => {
+      const VIMEO_ID = await route.params.filepath.videoIdTrimmed;
+      fetch(`${route.params.filepath.videoId}`)
+        .then(res => res.json())
+        .then(res =>
+          setVideo({
+            thumbnailUrl: "res.video.thumbs['640']",
+            videoUrl:
+              res.request.files.hls.cdns[res.request.files.hls.default_cdn].url,
+            video: "res.video",
+          }),
+        );
+    })();
   }, []);
   return (
     <SafeAreaView style={{flex: 1}}>
       <VideoPlayer
-        ref={ref => {
-          this.player = ref;
-        }}
+        // ref={ref => {
+        //   this.player = ref;
+        // }}
         source={{uri: video.videoUrl}}
         navigator={navigation}
         fullscreen={false}
