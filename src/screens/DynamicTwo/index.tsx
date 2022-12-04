eeea41bb3ef07d88238fff3703d573b9ff47acd2
Marios Kyriacou <60070568+imdarkk@users.noreply.github.com>
2022-12-04 22:37:04 +0200

    dynamic content

--- src/screens/DynamicTwo/index.tsx
+++ src/screens/DynamicTwo/index.tsx
@@ -1,9 +1,12 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useFocusEffect } from '@react-navigation/native';
 import React, {useState, useEffect} from 'react';
-import {View, StyleSheet, Text, SafeAreaView} from 'react-native';
+import {View, StyleSheet, Text, SafeAreaView, ScrollView} from 'react-native';
 import BackButton from '../../components/BackButton';
 import latestItems from '../../functions/latestItems';
+
+// Components
+import DynamicContentContainer from '../../components/DynamicContentContainer';
 
 export default function DynamicTwo() {
   const [guides, setGuides] = useState<any>([]);
@@ -14,21 +17,38 @@ export default function DynamicTwo() {
       (async() => {
         const items = await latestItems();
 
-        setGuides(items[0][0]);
-        setVideos(items[0][1]);
+        setGuides(items[0]);
+        setVideos(items[1]);
       })();
     }, [])
   );
 
   return (
     <SafeAreaView>
-      <BackButton text={'Latest Releases'} />
-      <View style={styles.wrapper}>
-        <Text style={styles.title}>Latest Guides</Text>
-        {/* MAP GUIDES */}
-        <Text style={styles.title}>Latest Videos</Text>
-        {/* MAP VIDEOS */}
-      </View>
+      <ScrollView>
+        <BackButton text={'Latest Releases'} />
+        <View style={styles.wrapper}>
+          <Text style={styles.title}>Latest Guides</Text>
+          {/* MAP GUIDES */}
+          {guides.map((guide: any) => {
+            const excerpt = guide?.excerpt?.rendered.replace(/(<([^>]+)>)/gi, '');
+            const title = guide?.title.rendered;
+            const mediaId = guide?.featured_media;
+
+            return (
+              <DynamicContentContainer
+                key={mediaId}
+                title={title}
+                excerpt={excerpt}
+                mediaId={mediaId}
+              />
+            )
+          })}
+
+          <Text style={styles.title}>Latest Videos</Text>
+          {/* MAP VIDEOS */}
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -37,6 +57,7 @@ const styles = StyleSheet.create({
   wrapper: {
     marginTop: 50,
     marginLeft: 32,
+    marginRight: 32
   },
   title: {
     color: '#11535C',
