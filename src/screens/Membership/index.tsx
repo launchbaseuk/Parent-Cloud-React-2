00bca0758e5ec7a11626f1ab7a448d535a6b6c84
dusanborne <dusan@borneagency.com>
2022-12-12 14:48:53 +0100

    ui fix

--- src/screens/Membership/index.tsx
+++ src/screens/Membership/index.tsx
@@ -9,102 +9,119 @@ const {width, height} = Dimensions.get('window');
 export default function Membership() {
   return (
     <SafeAreaView>
-      <BackButton text="Membership" />
-      <View style={{height: 32}} />
+      <View style={styles.wrapper}>
+        <BackButton text="Membership" />
+        <View style={{height: 32}} />
 
-      <View style={styles.container}>
-        <Text
-          style={{
-            fontFamily: 'Montserrat-Regular',
-            color: '#150E00',
-            fontSize: 16,
-            width: '30%',
-          }}>
-          Membership
-        </Text>
-        <Text
-          style={{
-            fontFamily: 'SofiaProBlack',
-            color: '#11535C',
-            fontSize: 16,
-            marginLeft: 26,
-          }}>
-          Corporate Plus
-        </Text>
-      </View>
-      <View style={styles.container}>
-        <Text
-          style={{
-            fontFamily: 'Montserrat-Regular',
-            color: '#150E00',
-            fontSize: 16,
-            width: '30%',
-          }}>
-          Subscription
-        </Text>
-        <Text
-          style={{
-            fontFamily: 'SofiaProBlack',
-            color: '#11535C',
-            fontSize: 16,
-            marginLeft: 26,
-          }}>
-          Sub Account
-        </Text>
-      </View>
-      <View style={styles.container}>
-        <Text
-          style={{
-            fontFamily: 'Montserrat-Regular',
-            color: '#150E00',
-            fontSize: 16,
-            width: '30%',
-          }}>
-          Status
-        </Text>
-        <Text
-          style={{
-            fontFamily: 'SofiaProBlack',
-            color: '#11535C',
-            fontSize: 16,
-            marginLeft: 26,
-          }}>
-          Active
-        </Text>
-      </View>
-      <View style={styles.container}>
-        <Text
-          style={{
-            fontFamily: 'Montserrat-Regular',
-            color: '#150E00',
-            fontSize: 16,
-            width: '30%',
-          }}>
-          Created
-        </Text>
-        <Text
-          style={{
-            fontFamily: 'SofiaProBlack',
-            color: '#11535C',
-            fontSize: 16,
-            marginLeft: 26,
-          }}>
-          01/2022
-        </Text>
+        <View style={styles.itemWrapper}>
+          <View style={styles.insideContainer}>
+            <Text
+              style={{
+                fontFamily: 'Montserrat-Regular',
+                color: '#150E00',
+                fontSize: 16,
+                width: '50%',
+              }}>
+              Membership
+            </Text>
+            <Text
+              style={{
+                fontFamily: 'SofiaProBlack',
+                color: '#11535C',
+                fontSize: 16,
+                marginLeft: 26,
+              }}>
+              Corporate Plus
+            </Text>
+          </View>
+        </View>
+        <View style={styles.itemWrapper}>
+          <View style={styles.insideContainer}>
+            <Text
+              style={{
+                fontFamily: 'Montserrat-Regular',
+                color: '#150E00',
+                fontSize: 16,
+                width: '50%',
+              }}>
+              Subscription
+            </Text>
+            <Text
+              style={{
+                fontFamily: 'SofiaProBlack',
+                color: '#11535C',
+                fontSize: 16,
+                marginLeft: 26,
+              }}>
+              Sub Account
+            </Text>
+          </View>
+        </View>
+        <View style={styles.itemWrapper}>
+          <View style={styles.insideContainer}>
+            <Text
+              style={{
+                fontFamily: 'Montserrat-Regular',
+                color: '#150E00',
+                fontSize: 16,
+                width: '50%',
+              }}>
+              Status
+            </Text>
+            <Text
+              style={{
+                fontFamily: 'SofiaProBlack',
+                color: '#11535C',
+                fontSize: 16,
+                marginLeft: 26,
+              }}>
+              Active
+            </Text>
+          </View>
+        </View>
+        <View style={styles.itemWrapper}>
+          <View style={styles.insideContainer}>
+            <Text
+              style={{
+                fontFamily: 'Montserrat-Regular',
+                color: '#150E00',
+                fontSize: 16,
+                width: '50%',
+              }}>
+              Created
+            </Text>
+            <Text
+              style={{
+                fontFamily: 'SofiaProBlack',
+                color: '#11535C',
+                fontSize: 16,
+                marginLeft: 26,
+              }}>
+              01/2022
+            </Text>
+          </View>
+        </View>
       </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  itemWrapper: {
+    width: width - 40,
+    paddingVertical: 10,
+    backgroundColor: '#F2F2F280',
+    // flexDirection: 'row',
+    alignSelf: 'center',
+    marginTop: 4,
+  },
+  wrapper: {
+    backgroundColor: '#ffffff',
+    height: '100%',
+  },
+  insideContainer: {
     flexDirection: 'row',
     alignItems: 'center',
     paddingLeft: 16,
-    width: width - 40,
-    height: 44,
-    backgroundColor: '#F2F2F280',
-    alignSelf: 'center',
-    marginTop: 4,
   },
 });
