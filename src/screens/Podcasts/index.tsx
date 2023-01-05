f7c4794be3c52de904ecc880924543e0d9321f07
dusanborne <dusan@borneagency.com>
2023-01-05 12:54:41 +0100

    image to podcast details added

--- src/screens/Podcasts/index.tsx
+++ src/screens/Podcasts/index.tsx
@@ -95,7 +95,6 @@ export default function Podcasts({navigation}: any) {
                 const link = podcast?.content?.rendered?.match(
                   /<img[^>]+src="?([^"\s]+)"?[^>]*>/,
                 );
-                //<audio controls
 
                 return (
                   <MediaListItem
